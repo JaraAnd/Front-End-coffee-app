4ab9ba0094c165b72a037699d302c494b1a99d36
Andres Jaramillo <79062604+JaraAnd@users.noreply.github.com>
2021-10-02 21:55:27 -0700

    Add files via upload

--- datastore.js
+++ datastore.js
@@ -1,28 +1,26 @@
 (function(window) {
-    "use strict";
-    var App = window.App || {};
-  
-    function DataStore() {
-      this.data = {};
-    }
+  "use strict";
+  var App = window.App || {};
 
-    DataStore.prototype.get = function(key) {
-        return this.data[key];
-      };
-      DataStore.prototype.getAll = function() {
-        return this.data;
-      };
-  
-    DataStore.prototype.add = function(key, val) {
-      this.data[key] = val;
-    };
-  
-    
-  
-    DataStore.prototype.remove = function(key) {
-      delete this.data[key];
-    };
-  
-    App.DataStore = DataStore;
-    window.App = App;
-  })(window);
+  function DataStore() {
+    this.data = {};
+  }
+
+  DataStore.prototype.add = function(key, val) {
+    this.data[key] = val;
+  };
+
+  DataStore.prototype.get = function(key) {
+    return this.data[key];
+  };
+  DataStore.prototype.getAll = function() {
+    return this.data;
+  };
+
+  DataStore.prototype.remove = function(key) {
+    delete this.data[key];
+  };
+
+  App.DataStore = DataStore;
+  window.App = App;
+})(window);
--- formhandler.js
+++ formhandler.js
@@ -36,6 +36,7 @@
     };
   
    
+  
     App.FormHandler = FormHandler;
     window.App = App;
   })(window);
